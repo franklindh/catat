a138735df98400d3a3e6fcff96d49456fbdb0122
franklindh <franklin.david@si.ukdw.ac.id>
2025-09-11 14:06:27 +0900

    refactor: main test

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,46 +6,25 @@ import (
 	"os"
 	"testing"
 
+	"github.com/franklindh/catat/util"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/joho/godotenv"
 )
 
 var testQueries *Queries
 var testDB *pgxpool.Pool
 
 func TestMain(m *testing.M) {
-	err := godotenv.Load("../../.env")
+
+	config, err := util.LoadConfig("../..")
 	if err != nil {
-		log.Println("Warning: Error loading .env file, using default environment variables")
+		log.Fatal("cannot load config:", err)
 	}
-
-	dbURL := os.Getenv("DB_URL")
-	if dbURL == "" {
-		host := getEnv("DB_HOST", "localhost")
-		port := getEnv("DB_PORT", "5432")
-		user := getEnv("DB_USER", "postgres")
-		password := getEnv("DB_PASSWORD", "password")
-		dbname := getEnv("DB_NAME", "testdb")
-		sslmode := getEnv("DB_SSLMODE", "disable")
-
-		dbURL = "postgresql://" + user + ":" + password + "@" + host + ":" + port + "/" + dbname + "?sslmode=" + sslmode
-	}
-
-	testDB, err = pgxpool.New(context.Background(), dbURL)
+	testDB, err = pgxpool.New(context.Background(), config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
-	defer testDB.Close()
 
 	testQueries = New(testDB)
 
 	os.Exit(m.Run())
-}
-
-func getEnv(key, defaultValue string) string {
-	value := os.Getenv(key)
-	if value == "" {
-		return defaultValue
-	}
-	return value
 }
